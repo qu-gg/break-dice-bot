1a7591aaae2e2458463294c1cdaac6aec6af4b83
Ferretsroq <Ferretsroq@gmail.com>
2024-01-14 17:10:51 -0500

    Oracle implementation

--- generatesettlement.py
+++ generatesettlement.py
@@ -1,5 +1,22 @@
 import discord
 import random
+import json
+import os
+
+#ironswornFile = open('./data/ironsworn_oracles.json', 'r', encoding='utf8')
+#ironswornData = json.load(ironswornFile)
+#ironswornFile.close()
+#starforgedFile = open('./data/starforged_oracles.json', 'r', encoding='utf8')
+#starforgedData = json.load(starforgedFile)
+#starforgedFile.close()
+data = []
+for file in os.listdir('./data'):
+    if file.endswith('.json'):
+        fileObj = open(f'./data/{file}', 'r', encoding='utf8')
+        fileData = json.load(fileObj)
+        fileObj.close()
+        data = data + fileData
+
 
 names = ['Lorem',
         'ipsum',
@@ -54,7 +71,7 @@ names = ['Lorem',
 
 class GenerateSettlementButtons(discord.ui.View):
     def __init__(self, size):
-        super().__init__()
+        super().__init__(timeout=None)
         self.settlement = Settlement(size)
         self.ViewSettlement()
 
@@ -75,10 +92,10 @@ class GenerateSettlementButtons(discord.ui.View):
     
     async def ViewContent(self, interaction, embeds, contents):
         self.clear_items()
-        self.add_item(ViewSettlementButton(label='View Settlement'))
         if len(contents) > 0:
             for content in contents:
                 self.add_item(content.button)
+        self.add_item(ViewSettlementButton(label='View Settlement'))
         await interaction.response.edit_message(embeds=embeds, view=self)
 
 class DistrictButton(discord.ui.Button):
@@ -139,11 +156,14 @@ class Cell:
 
     def NewCell(row, col):
         cell = Cell()
-        cell.name = random.choice(names)
+        districtPurpose = RollOnOracle(GetOracle(data, 'Starforged/Oracles/Settlements/Projects'))
+        #nonComm = OracleFromOracle(GetOracle(data, 'Spectacular_Settlements/Points_of_Interest/Non-Commercial_Location_Type/Non-Commercial Location Type'))
+        #nonCommDetail = RollOnOracle(GetOracle(data, OracleFromOracle(GetOracle(data, 'Spectacular_Settlements/Oracles/Points_of_Interest/Non-Commercial_Location_Type/Non-Commercial Location Type'))))
+        cell.name = districtPurpose
         cell.color = int(hex(random.randrange(0, 2**24)), 16)
-        cell.contents.append(CellContent(random.choice(names), 'District Content Placeholder', int(hex(random.randrange(0, 2**24)), 16)))
-        cell.contents.append(CellContent(random.choice(names), 'District Content Placeholder', int(hex(random.randrange(0, 2**24)), 16)))
-        cell.contents.append(CellContent(random.choice(names), 'District Content Placeholder', int(hex(random.randrange(0, 2**24)), 16)))
+        cell.contents.append(CellContent(RollOnOracle(GetOracle(data, OracleFromOracle(GetOracle(data, 'Spectacular_Settlements/Oracles/Points_of_Interest/Non-Commercial_Location_Type/Non-Commercial Location Type')))), 'District Content Placeholder', int(hex(random.randrange(0, 2**24)), 16)))
+        cell.contents.append(CellContent(RollOnOracle(GetOracle(data, OracleFromOracle(GetOracle(data, 'Spectacular_Settlements/Oracles/Points_of_Interest/Non-Commercial_Location_Type/Non-Commercial Location Type')))), 'District Content Placeholder', int(hex(random.randrange(0, 2**24)), 16)))
+        cell.contents.append(CellContent(RollOnOracle(GetOracle(data, OracleFromOracle(GetOracle(data, 'Spectacular_Settlements/Oracles/Points_of_Interest/Non-Commercial_Location_Type/Non-Commercial Location Type')))), 'District Content Placeholder', int(hex(random.randrange(0, 2**24)), 16)))
 
         cell.contents[0].AddContent(random.choice(names), 'Detail Placeholder', int(hex(random.randrange(0, 2**24)), 16))
         cell.contents[0].AddContent(random.choice(names), 'Detail Placeholder', int(hex(random.randrange(0, 2**24)), 16))
@@ -174,3 +194,72 @@ class CellContent:
         newContent = CellContent(name, description, color)
         self.contents.append(newContent)
         self.button = ContentButton(label=self.name, embeds=self.toEmbed(True), contents=self.contents)
+
+def GetOracle(data, id):
+    splitID = id.split('/')
+    parentOracle = list(filter(lambda oracle: oracle['$id'].startswith(f'{splitID[0]}/{splitID[1]}/{splitID[2]}'), data))[0]
+    depth = 3
+    for oracle in parentOracle['Oracles']:
+        if oracle['$id'] == id:
+            return oracle['Table']
+    while 'Oracles' in parentOracle.keys():
+        depth += 1
+        depthString = ''
+        for split in range(depth):
+            depthString += f'{splitID[split]}/'
+        depthString = depthString.strip('/')
+        parentOracle = list(filter(lambda oracle: oracle['$id'].startswith(depthString), parentOracle['Oracles']))[0]
+        for oracle in parentOracle['Oracles']:
+            if oracle['$id'] == id:
+                return oracle['Table']
+    
+    
+    #if 'Oracles' in parentOracle['Oracles']:
+    #    for oracle in parentOracle['Oracles']['Oracles']:
+    #        if oracle['$id'] == id:
+    #            return oracle['Table']
+    return {}
+        
+def RollOnOracle(oracle):
+    result = '⏵'
+    while('⏵' in result):
+        roll = random.randrange(1, oracle[-1]['Ceiling'])
+        for entry in oracle:
+            if roll >= entry['Floor'] and roll <= entry['Ceiling']:
+                result = entry['Result']
+            
+    return result
+
+def OracleFromOracle(oracle):
+    roll = random.randrange(1, oracle[-1]['Ceiling'])
+    for entry in oracle:
+        if roll >= entry['Floor'] and roll <= entry['Ceiling']:
+            return entry['Oracle rolls'][0]
+    return ''
+
+if __name__ == '__main__':
+    #nameType = OracleFromOracle(GetOracle(ironswornData, 'Ironsworn/Oracles/Settlement/Name'))
+    #while nameType.endswith('Something_Else'):
+    #    nameType = OracleFromOracle(GetOracle(ironswornData, 'Ironsworn/Oracles/Settlement/Name'))
+
+    #name = RollOnOracle(GetOracle(ironswornData, nameType))
+    #quickNamePrefix = RollOnOracle(GetOracle(ironswornData, 'Ironsworn/Oracles/Settlement/Quick_Name/Prefix'))
+    #quickNameSuffix = RollOnOracle(GetOracle(ironswornData, 'Ironsworn/Oracles/Settlement/Quick_Name/Suffix'))
+    #trouble = RollOnOracle(GetOracle(ironswornData, 'Ironsworn/Oracles/Settlement/Trouble'))
+
+    #for i in range(10000):
+    #    project = RollOnOracle(GetOracle(data, 'Starforged/Oracles/Settlements/Projects'))
+    #print(nameType)
+    #print(name)
+    #print(quickNamePrefix)
+    #print(quickNameSuffix)
+    #print(trouble)
+    #    print(project)
+    pass
+    #nonComm = OracleFromOracle(GetOracle(data, 'Spectacular_Settlements/Oracles/Points_of_Interest/Non-Commercial_Location_Type/Non-Commercial Location Type'))
+    #print(nonComm)
+    nonCommDetail = RollOnOracle(GetOracle(data, OracleFromOracle(GetOracle(data, 'Spectacular_Settlements/Oracles/Points_of_Interest/Non-Commercial_Location_Type/Non-Commercial Location Type'))))
+    print(nonCommDetail)
+
+
+
